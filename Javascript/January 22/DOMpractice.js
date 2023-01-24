aede2c589b71de3297110d3535e3625a481ffe98
Cosmin <cosmingombos@gmail.com>
2023-01-24 22:02:12 +0200

    Finally Finished Task List
    At least the Javascript, still need to add CSS tomorrow

--- Javascript/January 22/DOMpractice.js
+++ Javascript/January 22/DOMpractice.js
@@ -1,19 +1,40 @@
 var input = document.getElementById("userInput");
 var button = document.getElementById("btn");
+var button2 = document.querySelectorAll(".btn2");
 var ul = document.querySelector("ul");
 var list = document.getElementsByClassName("line-through");
 
 
+
+
+
+function toggleList(){
+    this.classList.toggle("line-through")
+}
+
 //add items to list
 function addItem() {
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
+    li.append(document.createTextNode(input.value));
+    li.classList.add('list-elements')
+    li.addEventListener("click", toggleList)
     ul.appendChild(li);
     input.value = "";
+
+    var btnDelete = document.createElement("button");
+    btnDelete.innerHTML = "Delete"
+    li.append(btnDelete);
+    btnDelete.addEventListener("click", ()=>{
+        li.remove();
+    })
+
+
+
 }
 
 function addItemBtn(event) {
-    if (input.value.length != 0 && event.code === "Enter") { addItem() }
+    if (input.value.length != 0 && event.code === "Enter") 
+    { addItem() }
 }
 
 function addItemClick() {
@@ -21,6 +42,11 @@ function addItemClick() {
         addItem();
     }
 }
+
+button.addEventListener("click", addItemClick);
+
+input.addEventListener("keypress", addItemBtn);
+
 
 document.addEventListener("DOMContentLoaded", () => {
     //  Execute the code after the HTML content is loaded
@@ -52,6 +78,4 @@ document.addEventListener("DOMContentLoaded", () => {
 //  Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
 // When adding a new list item, it automatically adds the delete button next to it (hint: be sure to check if new items are clickable too!)
 
-button.addEventListener("click", addItemClick);
 
-input.addEventListener("keypress", addItemBtn);
