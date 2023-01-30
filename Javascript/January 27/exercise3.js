cdef650740e1e0a9a8e119e1684d78d6726a4c0c
Cosmin <cosmingombos@gmail.com>
2023-01-30 23:24:09 +0200

    Finished half of Javascript exercises
    Javascript and command line, how does a day in the life of a dev look like

--- Javascript/January 27/exercise3.js
+++ Javascript/January 27/exercise3.js
@@ -1,9 +1,9 @@
 // change everything below to the newer Javascript!
 
 // let + const
-var a = 'test';
-var b = true;
-var c = 789;
+let a = 'test';
+const b = true;
+const c = 789;
 a = 'test2';
 
 
@@ -20,21 +20,17 @@ var lastName = person.lastName;
 var age = person.age;
 var eyeColor = person.eyeColor;
 
+const {firstName, lastName, age, eyeColor} = person;
 
 // Object properties
 var a = 'test';
 var b = true;
 var c = 789;
 
-var okObj = {
-  a: a,
-  b: b,
-  c: c
-};
-
+var okObj = {a,b,c,};
 
 // Template strings
-var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+var message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???";`
 
 
 // default arguments
@@ -43,8 +39,12 @@ function isValidAge(age) {
     return age
 }
 
+const isValidAge = (age = 10) => age;
+
 // Symbol
 // Create a symbol: "This is my first Symbol"
+
+let sym = Symbol(hello);
 
 // Arrow functions
 function whereAmI(username, location) {
@@ -53,4 +53,8 @@ function whereAmI(username, location) {
     } else {
         return "I am totally lost!";
     }
+}
+
+const whereAmI = (username, location) =>{
+    username&&location?`I'm not lost!`:`I'm totally lost!`
 }
--- Javascript/January 27/exercise5.js
+++ Javascript/January 27/exercise5.js
@@ -28,23 +28,52 @@ const array = [
 ];
 
 //Create an array using forEach that has all the usernames with a "!" to each of the usernames
-
+let newArray = []
+array.forEach(user =>{
+    let {username} = user;
+  username = username + '!'
+    newArray.push(username); 
+})
 
 //Create an array using map that has all the usernames with a "? to each of the usernames
-
+const mapArray = array.map(user => {
+	let { username } = user;
+	return username + "?";
+})
+console.log(mapArray);
 
 //Filter the array to only include users who are on team: red
+const newArray2 = array.filter(item =>{
+    return item.team === 'red'
+})
+
 
 
 //Find out the total score of all users using reduce
+
+const total = array.reduce((total, item) =>{
+    return total + item.score;
+    
+}, 0) 
+console.log(`Total Score for now is: ${total}`);
+
+
 
 // (1), what is the value of i?
 // (2), Make this map function pure:
 const arrayNum = [1, 2, 4, 5, 8, 9];
 const newArray = arrayNum.map((num, i) => {
-	console.log(num, i);
-	alert(num);
 	return num * 2;
 })
 
+// value of i = index, 0 then 1 then 2 and so on
+
+
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+
+const newList = array.map(user =>{
+  user.items = user.items.map(item => {
+    return item + '!';
+  })
+  return user;
+})
