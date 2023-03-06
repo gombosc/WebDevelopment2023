c12d510e45b84ecd113397378f261fea9051884b
Cosmin <cosmingombos@gmail.com>
2023-03-06 23:46:46 +0100

    moving along with the robofriends project added robots.js file created card.js and added three robots modified index.js and used destructuring to add the items from robots.js to the cards added tachyonds packet, still need to research it added explanative notes '

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import {robots} from './robots';    // if the import is not default we need to use destructuring, which is made in brackets {}
+import 'tachyons';    // we use tachyons to style elements directly, represents a package
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>    {/* the elements inside the brackets represents the props, for example id, name and email are props*/}
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    </div>
   </React.StrictMode>
 );
 
