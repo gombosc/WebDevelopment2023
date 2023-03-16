47e00e44180a25bdb808fd11a9b47d6214e0f634
Cosmin <cosmingombos@gmail.com>
2023-03-16 18:36:42 +0100

    added API and other stufff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
-import {robots} from './robots';
+// import {robots} from './robots';
 import'./App.css';
+import Scroll from "./Scroll";
 
 // props are simply things that come out of state
 //  State >> props
@@ -11,9 +12,14 @@ class App extends Component {
     constructor() {
     super()
     this.state = {
-        robots: robots,
+        robots: [],
         searchField: ''
     }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => {this.setState({robots:users})});
+        // fetching users from API
     }
 
     onSearchChange = (event) =>{
@@ -23,14 +29,21 @@ class App extends Component {
     render(){
         const filteredRobots = this.state.robots.filter(robots =>{
         return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())})
-    return (
+            
+        if(this.state.robots.lenght === 0){
+            return <h1>Loading</h1>;
+        }
+        else {
+        return (
         <div className="tc">
         <h1 className="f1">Robofriends</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
+        <Scroll>
+            <CardList robots={filteredRobots}/>
+        </Scroll>
         </div>
     )
   };
-}
+}}
 
 export default App;
