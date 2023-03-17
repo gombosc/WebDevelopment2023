5e342d3bcf7d84de7bd9953918367e5464b0340a
Cosmin <cosmingombos@gmail.com>
2023-03-17 23:47:44 +0100

    solved the freaking error, it was due to the ternary for robots having a negation before Also created ErrorBoundary and checked if its work Still need to add Github Pages

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) =>{
+  // if (true){
+    // throw new Error("Error detected"); ----- test ErrorBoundary
+  // }
   return(
   <>
     {
@@ -21,4 +24,4 @@ const CardList = ({robots}) =>{
 
 export default CardList;
 
-{/* <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>    the elements inside the brackets represents the props, for example id, name and email are props */}
+// {/* <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>    the elements inside the brackets represents the props, for example id, name and email are props */}
--- src/container/App.js
+++ src/container/App.js
@@ -2,9 +2,10 @@ import React, {Component} from "react";
 import SearchBox from "../components/SearchBox";
 import CardList from "../components/Cardlist";
 // import {robots} from './robots';
-import'../container/App.css';
-import Scroll from "../components/Scroll";
+import "./App.css";
+import Scroll from '../components/Scroll';
 import "../container/SEGA.woff"
+import ErrorBoundary from "../components/ErrorBoundary";
 
 // props are simply things that come out of state
 //  State >> props
@@ -28,18 +29,22 @@ class App extends Component {
     }
     
     render(){
-        const {robots, searchField} = this.state;
-        const filteredRobots = robots.filter(robots =>{
-        return robots.name.toLowerCase().includes(searchField.toLowerCase())})
+        const {robots, searchField} = this.state; 
+        // deconstructuring for easier state use
+        const filteredRobots = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())})
         
-        return !robots.lenght ?
+        return robots.lenght?
+        // using ternary operators for easier understanding
         <h1>Loading</h1>:
         (
         <div className="tc">
         <h1 className="f1">Robofriends</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
         <Scroll>
-            <CardList robots={filteredRobots}/>
+            <ErrorBoundary>
+                <CardList robots={filteredRobots}/>
+            </ErrorBoundary>
         </Scroll>
         </div>
     )
