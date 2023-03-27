308bf4c76d789348340df527d992958ee403e56c
Cosmin <cosmingombos@gmail.com>
2023-03-27 23:30:32 +0200

    Implemented API to the input element, it now shows information about the chosen number Lost two hours trying to figure out why response.json wasn't working, found out It was actually plain text and needed to use response.text()

--- Javascript/March 24th (Numbers API Project)/script.js
+++ Javascript/March 24th (Numbers API Project)/script.js
@@ -1,9 +1,28 @@
-buttonMain = document.getElementById("start-btn");
-divTrivia = document.getElementById("NumbersTrivia");
+const buttonMain = document.getElementById("start-btn");
+const divTrivia = document.getElementById("NumbersTrivia");
+const inputValue = document.getElementById("numberValue");
+const btnValue= document.getElementById("btnValue");
+const apiResponse = document.getElementById("ApiResponse")
 
 
 function showNumbersTrivia(){
     divTrivia.hidden = !divTrivia.hidden;
     // buttonMain.remove()
-
 }
+
+async function fetchFromApi(inputValue){
+    const response = await fetch(`http://numbersapi.com/${inputValue}/math`);
+    const data = await response.text();
+    // spent way too much time figuring out this error
+    apiResponse.innerHTML = data;
+    console.log(data);
+    }
+    
+ btnValue.addEventListener("click", ()=>{
+    const valueFromInput = inputValue.value;
+    fetchFromApi(valueFromInput);
+    console.log("The Input Value is: " , valueFromInput);
+ })
+
+
+
