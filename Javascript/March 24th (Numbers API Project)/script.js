44903baaaf0f04af19233be754cc7bd2dfb81701
Cosmin <cosmingombos@gmail.com>
2023-03-29 17:34:18 +0200

    slowly but surely progressing need to add different pages for every step instead of doing it all on one page

--- Javascript/March 24th (Numbers API Project)/script.js
+++ Javascript/March 24th (Numbers API Project)/script.js
@@ -1,21 +1,37 @@
-const buttonMain = document.getElementById("start-btn");
+// start button and container for it 
+const buttonStart = document.getElementById("start-btn");
+const startBtnContainer = document.getElementById("StartBtnContainer")
+
 const divTrivia = document.getElementById("NumbersTrivia");
 const inputValue = document.getElementById("numberValue");
 const btnValue= document.getElementById("btnValue");
 const apiResponse = document.getElementById("ApiResponse")
 
 
+function showOptions(){
+
+}
+
+// Show options after main button is pressed
 function showNumbersTrivia(){
     divTrivia.hidden = !divTrivia.hidden;
-    // buttonMain.remove()
+    welcomeText = document.getElementById("Welcome").innerHTML = "Numbers Trivia"
+    /
+    startBtnContainer.remove()
 }
 
 async function fetchFromApi(inputValue){
+    try{
     const response = await fetch(`http://numbersapi.com/${inputValue}/math`);
     const data = await response.text();
     // spent way too much time figuring out this error
-    apiResponse.innerHTML = data;
+    apiResponse.textContent = data;
     console.log(data);
+        }
+        catch(error){
+            console.error("Fetching Error: ", error);
+            apiResponse.textContent = "Failed Loading Text..."
+        }
     }
     
  btnValue.addEventListener("click", ()=>{
